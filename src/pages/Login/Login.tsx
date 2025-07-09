40795fb52385f263512edda71444cc582de3ccca
habulfeilat <habulfeilat@ejada.com>
2025-07-09 15:42:17 +0300

    Fix Login functionality

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,12 +4,10 @@ import { AuthContext } from "../../context/AuthContext";
 import type { FormLoginValues } from "../../types/types";
 import { Controller, useForm } from "react-hook-form";
 
-
 const LoginForm = () => {
+  const { handleSubmit, register, control } = useForm<FormLoginValues>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const context = useContext(AuthContext);
-
-
 
   const onSubmit = async (data: FormLoginValues) => {
     setIsLoading(true);
@@ -47,7 +45,6 @@ const LoginForm = () => {
   return (
     <>
       <div className="flex max-w-1440 mx-auto min-h-screen">
-
         <div className=" flex-1 flex flex-col  justify-center items-center relative">
           <div className="max-w-[360px]">
             <div className="mb-10">
@@ -55,47 +52,82 @@ const LoginForm = () => {
             </div>
             <div className="mb-8">
               <div className="font-bold text-4xl mb-3">Login</div>
-              <div className="text-gray-600">Welcome back! Please enter your details.</div>
+              <div className="text-gray-600">
+                Welcome back! Please enter your details.
+              </div>
             </div>
 
-            <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-5">
                 <label className="">
-                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">Email</div>
+                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">
+                    Email
+                  </div>
                   <input
                     type="text"
                     placeholder="Enter your email"
                     className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4"
-                    {...useForm<FormLoginValues>().register("name", { required: true })}
+                    {...register("name", {
+                      required: true,
+                    })}
                   />
                 </label>
               </div>
               <div className="mb-6">
                 <label className="">
-                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">Password</div>
+                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">
+                    Password
+                  </div>
                   <input
                     type="password"
-                    placeholder="Enter your email"
+                    placeholder="Enter your password"
                     className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4"
-                    {...useForm<FormLoginValues>().register("name", { required: true })}
+                    {...register("password", {
+                      required: true,
+                    })}
                   />
                 </label>
               </div>
 
               <div className="flex justify-between mb-6">
                 <label className="flex items-center gap-2">
-                  <input type="checkbox" className="w-4 h-4 border-gray-300" />
+                  <Controller
+                    name="remember"
+                    control={control}
+                    defaultValue={false}
+                    render={({ field }) => (
+                      <input
+                        type="checkbox"
+                        className="w-4 h-4 border-gray-300"
+                        checked={field.value}
+                        {...field}
+                      />
+                    )}
+                  />
                   Remember me
                 </label>
-                <a href="#" className="text-blue-700 font-bold hover:underline">Forgot Password?</a>
+                <a href="#" className="text-blue-700 font-bold hover:underline">
+                  Forgot Password?
+                </a>
               </div>
 
-              <button className="text-white font-bold bg-blue-700 p-2.5 w-full rounded-lg">Sign in</button>
-            </div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="text-white font-bold bg-blue-700 p-2.5 w-full rounded-lg"
+              >
+                Sign in
+              </button>
+            </form>
 
             <div className="mt-9 text-sm text-gray-600 text-center">
               Don’t have an account?
-              <a href="#" className="text-blue-700 font-bold hover:underline mx-2">Sign up</a>
+              <a
+                href="#"
+                className="text-blue-700 font-bold hover:underline mx-2"
+              >
+                Sign up
+              </a>
             </div>
           </div>
 
@@ -104,18 +136,19 @@ const LoginForm = () => {
           </div>
         </div>
 
-
         <div className="bg-black flex-1 relative p-8">
-
           <div className="text-end">
             <button className="text-white inline-flex gap-2 items-center justify-center px-3 py-2 bg-gray-800 rounded-lg">
-              <img src="/src/assets/svg/life-buoy.svg" />
+              <img src="/src/assets/svg/life-buoy.svg" alt="Help" />
               Need Help!
             </button>
           </div>
 
-          <img src="/login_lower_logo.png" alt="Login Background" className="absolute bottom-16" />
-
+          <img
+            src="/login_lower_logo.png"
+            alt="Login Background"
+            className="absolute bottom-16"
+          />
         </div>
       </div>
     </>
