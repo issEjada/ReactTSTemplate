258bae1aaeb65226537bc6738c706cbc4fbf6894
Hamzeh Alayan <hamzehalayan01@gmail.com>
2025-07-22 14:51:08 +0300

    added about us page

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import AboutUsAlphas from '../assets/svg/aboutUsAlphas.svg?react'; 
+const AboutUs: React.FC = () => {
+  return (
+    <section className="bg-white text-gray-800 px-6 py-16 md:px-20">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-1 gap-12 items-start">
+        <div className="flex">
+        <div className="flex flex-col text-left w-[70%]">
+          <p className="text-blue-700 font-semibold mb-2 ">Why we’re different</p>
+          <h1 className="text-3xl md:text-4xl font-bold  text-gray-900 mb-4">
+            We bring honesty and transparency to anti-fraud services technology
+          </h1>
+          <blockquote className="italic text-lg text-gray-600 mb-6">
+            "As fraudsters evolve, so must their hunters."
+          </blockquote>
+                    </div>
+          <button className="bg-blue-700 w-[125px] h-[48px] text-white px-5 py-3 rounded-[8px] ml-auto hover:bg-blue-800 transition duration-300">Contact Us</button>
+            </div>
+        <div className="flex">
+        <div className="flex flex-col w-[50%] text-left">
+          <p className="text-[18px] leading-relaxed mb-4 font-weight-[400]">
+            ALPHAS delivers the region’s first advanced anti-fraud platform
+            engineered in Saudi Arabia. We arm governments, financial institutions, 
+            and enterprises with AI-driven defenses to combat evolving cyber 
+            threats—turning fraud prevention into a strategic advantage.
+          </p>
+          <h2 className="text-2xl font-bold mt-10 mb-4 ">Why we’re better</h2>
+          <p className="text-base text-gray-600 mb-4 ">
+            Unlike reactive systems, our suite predicts, scores, and neutralizes threats in real-time:
+          </p>
+
+          <ul className="list-disc pl-6 space-y-2 text-gray-600">
+            <li>
+              AI-Powered Vigilance: Behavioral pattern analysis, 
+              temporal sequencing, and anomaly detection.
+            </li>
+            <li>
+             Zero-Trust Architecture: End-to-end encryption, app hardening, 
+              and tamper-proof SDK integration.
+            </li>
+            <li>
+              Saudi-Hosted Sovereignty:All data resides in-Kingdom—complying 
+              with NCA, SAMA, and Vision 2030.
+            </li>
+          </ul>
+          </div>
+        <div className="flex justify-center">
+            {/* <img src={aboutUsAlphas} alt="Cyber Security Illustration"  className="w-full"/> */}
+            <AboutUsAlphas className="w-full"/>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
