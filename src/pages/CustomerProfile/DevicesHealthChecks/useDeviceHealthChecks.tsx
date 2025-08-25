52139c2276a5579e03fc08eacf674df9381f4538
MusaabGhazal-EJADA <mughazal@ejada.com>
2025-08-25 17:13:47 +0300

    customerProfile: fix sending no date

--- src/pages/CustomerProfile/DevicesHealthChecks/useDeviceHealthChecks.tsx
+++ src/pages/CustomerProfile/DevicesHealthChecks/useDeviceHealthChecks.tsx
@@ -31,8 +31,8 @@ export const useDevicesHealthChecks = () => {
     const data = {
       maxPageSize: 10,
       page: currentPage,
-      fromTimestamp: new Date(devicesHealthChecksFilterData.fromTimestamp || "").toISOString(),
-      toTimestamp: new Date(devicesHealthChecksFilterData.toTimestamp || "").toISOString(),
+      fromTimestamp: new Date(devicesHealthChecksFilterData.fromTimestamp || Date.now()).toISOString(),
+      toTimestamp: new Date(devicesHealthChecksFilterData.toTimestamp || Date.now()).toISOString(),
     };
 
     await CustomerClient.getDevicesHealthChecksData(data)
