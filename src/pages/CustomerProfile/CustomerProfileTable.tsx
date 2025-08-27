f7256286f3f55c911dda9646946fe61ea6dd0a18
habulfeilat <habulfeilat@ejada.com>
2025-08-27 15:17:37 +0300

    fix pagination

--- src/pages/CustomerProfile/CustomerProfileTable.tsx
+++ src/pages/CustomerProfile/CustomerProfileTable.tsx
@@ -354,7 +354,7 @@ export function CustomerProfileTable<TData extends object>({
                 className={`px-3 py-2 w-full sm:w-[87px] h-[36px] border border-gray-300 rounded-lg 
           ${
             currentPage === 1
-              ? "bg-gray-100 dark:text-white dark:bg-gray-800"
+              ? "bg-gray-100 dark:text-white dark:bg-gray-800 cursor-not-allowed"
               : "hover:bg-gray-100 text-black dark:text-white dark:hover:bg-gray-800"
           }`}
               >
@@ -370,7 +370,7 @@ export function CustomerProfileTable<TData extends object>({
                 className={`px-3 py-2 w-full sm:w-[60px] h-[36px] border border-gray-300 rounded-lg 
           ${
             currentPage === Math.ceil(totalCount / itemsPerPage)
-              ? "bg-gray-100 dark:text-white dark:bg-gray-800"
+              ? "bg-gray-100 dark:text-white dark:bg-gray-800 cursor-not-allowed"
               : "hover:bg-gray-100 text-black dark:text-white dark:hover:bg-gray-800"
           }`}
               >
