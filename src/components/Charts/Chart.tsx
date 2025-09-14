7d87d852cbcd4861a40ccf94e1e1e651c6d5973c
habulfeilat <habulfeilat@ejada.com>
2025-09-14 14:16:44 +0300

    temp code

--- src/components/Charts/Chart.tsx
+++ src/components/Charts/Chart.tsx
@@ -1,5 +1,5 @@
 import LineChartComponent from "./LineChartComponent";
-import PieChartComponent from "./PieChartComponent";
+// import PieChartComponent from "./PieChartComponent";
 type ChartProps = {
   chartType: "line" | "pie";
 };
@@ -8,8 +8,8 @@ export default function Chart({ chartType }: ChartProps) {
   switch (chartType) {
     case "line":
       return <LineChartComponent />;
-    case "pie":
-      return <PieChartComponent />;
+    // case "pie":
+    //   return <PieChartComponent />;
     default:
       return <div>No chart type selected</div>;
   }
